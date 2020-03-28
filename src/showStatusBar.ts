c70eb4eaa071557a2c287f53b94e8614ba7a9e66
ibyli <15186743693@163.com>
2020-03-28 11:50:30 +0800

    update: 更换logo

--- src/showStatusBar.ts
+++ src/showStatusBar.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 
 export default () => {
   const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 0);
-  statusBarItem.text = '❤ 陈红鼓励师';
+  statusBarItem.text = '❤ 召唤鼓励师';
   statusBarItem.tooltip = '召唤陈红鼓励师';
   statusBarItem.command = 'ch.showReminderView';
   statusBarItem.show();
